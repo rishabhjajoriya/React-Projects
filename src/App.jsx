7b904917cda5d8ea35fc3d7c3b8e9354f88078dc
Rishabh <rishabhjajoriya27@gmail.com>
2024-05-13 13:23:20 +0530

    new accordian

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Accordian from "./components/accordian/accordian";
+import Accordian2 from './components/Accordian-2/Accordian'
 import RandomColor from "./components/random-color";
 import StarRating from "./components/starRating";
 import ImageSlider from "./components/imageSlider";
@@ -13,6 +14,7 @@ import SearchAutocomplete from "./components/searchAutocomplete";
 import QRCodeGenerator from "./components/qr-code-generator";
 import ScrollIndicator from "./components/scrollIndicator";
 import TabTest from "./components/custom-tabs/tab-test";
+import data from './components/Accordian-2/data'
 // Correct casing for uppercase Test.jsx
 
 function App() {
@@ -21,11 +23,16 @@ function App() {
   return (
     <>
       {/* <ImageSlider url = {'https://picsum.photos/v2/list'} limit={'10'}/> */}
-      <StarRating noofstars={10} />
+      {/* <StarRating noofstars={10} /> */}
       {/* <RandomColor/> */}
       {/* <QRCodeGenerator /> */}
       {/* <ScrollIndicator url='https://dummyjson.com/products?limit=100' /> */}
       {/* <TabTest/> */}
+      
+      {data && data.map((dataitem) =>{
+       return <Accordian2 dataitem = {dataitem}/>
+      })}
+
     </>
   );
 }
